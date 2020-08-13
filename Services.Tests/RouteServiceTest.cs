49263ce5b928fca13ed833af21fc6171b8b94dad
Daniel Fonseca Reis <daniel.fonsecareis-ext@ab-inbev.com>
2020-08-13 11:56:44 -0300

    foundation

---
+++ Services.Tests/RouteServiceTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using Services;
+using System.Collections.Generic;
+using Core.Models;
+
+
+namespace Services.Tests
+{
+
+    public class RouteServiceTest
+    {
+        private readonly RouteService _routeService;
+
+        public RouteServiceTest()
+        {
+            _routeService = new RouteService();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            List<Route> routes = _routeService.GetAllRoutes("Test");
+        }
+    }
+}
