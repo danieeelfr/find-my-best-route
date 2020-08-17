66696fb50f7a9c0300909b60e930d92e092947c7
Daniel Fonseca Reis <daniel.fonsecareis-ext@ab-inbev.com>
2020-08-17 09:27:57 -0300

    foundation

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -13,23 +13,35 @@ namespace ConsoleApplication
                 return;
             }
 
-            var routeService = new RouteService();
-
-            Console.WriteLine("Please enter the route: ");
-            var route = Console.ReadLine().Split("-");
-
-            if (args.Length > 0 && route.Length == 2)
+            while (true) // Loop indefinitely
             {
-                var filePath = args[0];
-                var bestRoute = routeService.GetBestRoute($"{route[0]}", $"{route[1]}", filePath);
-                Console.WriteLine($"best route: {bestRoute}");
-            }
-            else
-            {
-                Console.WriteLine("Please try again informing a valid route (FROM,TO)...");
-            }
+                var routeService = new RouteService();
 
-            Console.ReadKey();
+                Console.WriteLine("Please enter the route: ");
+                var route = Console.ReadLine();
+
+                if (route.Equals("exit")) // Check string
+                {
+                    Console.WriteLine("Finished...");
+                    break;
+                }
+
+                var splittedRoute = route.Split("-");
+
+                if (args.Length > 0 && splittedRoute.Length == 2)
+                {
+                    var filePath = args[0];
+                    var bestRoute = routeService.GetBestRoute($"{splittedRoute[0]}", $"{splittedRoute[1]}", filePath);
+                    Console.WriteLine($"best route: {bestRoute}");
+                }
+                else
+                {
+                    Console.WriteLine("Please try again informing a valid route (FROM,TO) or type exit to finish...");
+                }
+
+                Console.ReadKey();
+
+            }
         }
     }
 }
